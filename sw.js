f5f10d8baacecd09a1ec9c26d104555249083eab
Alexey <8bitjoey@gmail.com>
2026-07-19 12:12:40 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const C='krys-v1';
+const A=['./','index.html','manifest.json','lib/jsqr.js','lib/three.min.js','icon-180.png','icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(r=>r||fetch(e.request)));});
